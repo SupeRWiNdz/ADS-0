1f3d9f0a63af8e47020c835982cbd850c163b334
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-02-17 20:31:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int x;
+	a > b ? x = a : x = b;
+	for (x; x>0; --x) {
+		if (a % x == 0 && b % x == 0) {
+			return x;
+		}
+	}
 }
