853dd5634747c5e7fd9db63472ecb9cee181bc04
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-02-18 13:19:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int x;
-	a > b ? x = a : x = b;
-	for (x; x>0; --x) {
-		if ((a % x == 0) && (b % x == 0)) {
-			return (x);
-		}
-	}
+int x;
+a > b ? x = a : x = b;
+for (x; x>0; --x) {
+if ((a % x == 0) && (b % x == 0)) {
+return (x);
+}
+return (0);
+}
 }
